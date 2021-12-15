b7e162f4c7773eff0ea9e0643a77cf75c45e4809
brabrahen <brabrahen@gmail.com>
2021-12-15 15:44:09 -0300

    header e inicial pronto

--- funimation/src/components/shared/header/header.js
+++ funimation/src/components/shared/header/header.js
@@ -1,18 +1,23 @@
 import "./header.css";
 import React from "react";
-import { MdManageSearch } from 'react-icons/md'
-import { GiBeastEye } from 'react-icons/gi'
+import { MdManageSearch } from "react-icons/md";
+import { GiBeastEye } from "react-icons/gi";
 
 const Nav = () => {
   return (
     <div className="todo">
-        <section className="header">
-            <div className="txt-header"> 
-            <span><GiBeastEye />Funimation</span>
-            <span><MdManageSearch />Pesquisar </span>
-            </div>
-        </section>
-        
+      <section className="header">
+        <div className="txt-header">
+          <span>
+            <GiBeastEye />
+            Funimation
+          </span>
+          <form>
+          <label className="search"><MdManageSearch /></label>
+          <input id="pesquisa" className="pesquisa" type="text" placeholder="Pesquise seu anime aqui"></input>      
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
